e9afebef1cb01bb88da9f5c01d0736a11aea9c1f
lsa26 <151360195+lsa26@users.noreply.github.com>
2025-05-17 20:38:26 +0200

    Update SetupAndroidBuild.cs

--- Assets/Editor/SetupAndroidBuild.cs
+++ Assets/Editor/SetupAndroidBuild.cs
@@ -10,41 +10,32 @@ public class SetupAndroidBuild
         try
         {
             UnityEngine.Debug.Log("Configuration de l'environnement de build Android...");
-            
-            // Configuration des paramètres Android
+
             PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel22;
             PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel33;
-            
-            // Configuration de l'application
+
             PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.yourcompany.angrybirdsremake");
             PlayerSettings.productName = "Angry Birds Remake";
             PlayerSettings.companyName = "Your Company";
             PlayerSettings.bundleVersion = "1.0.0";
             PlayerSettings.Android.bundleVersionCode = 1;
-            
-            // Configuration de la qualité
+
             PlayerSettings.Android.disableDepthAndStencilBuffers = false;
-            PlayerSettings.SetGraphicsAPIs(BuildTarget.Android, new UnityEngine.Rendering.GraphicsDeviceType[] { 
+            PlayerSettings.SetGraphicsAPIs(BuildTarget.Android, new UnityEngine.Rendering.GraphicsDeviceType[] {
                 UnityEngine.Rendering.GraphicsDeviceType.OpenGLES3,
                 UnityEngine.Rendering.GraphicsDeviceType.Vulkan
             });
-            
-            // Orientation
+
             PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeRight;
             PlayerSettings.allowedAutorotateToPortrait = false;
             PlayerSettings.allowedAutorotateToPortraitUpsideDown = false;
             PlayerSettings.allowedAutorotateToLandscapeRight = true;
             PlayerSettings.allowedAutorotateToLandscapeLeft = true;
-            
-            // Icône de l'application
-            // Note: Vous pouvez ajouter ici la configuration de l'icône si nécessaire
-            
-            // Configuration de la mémoire
+
             PlayerSettings.Android.preferredInstallLocation = AndroidPreferredInstallLocation.Auto;
-            
-            // Enregistrer les changements
+
             AssetDatabase.SaveAssets();
-            
+
             UnityEngine.Debug.Log("Configuration du projet terminée avec succès.");
         }
         catch (Exception e)
