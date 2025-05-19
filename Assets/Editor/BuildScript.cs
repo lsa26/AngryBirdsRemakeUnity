6b0f5acbd4ec975a2af4a55b1e82ed510a48bd2d
lsa26 <151360195+lsa26@users.noreply.github.com>
2025-05-19 08:30:24 +0200

    Update BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,84 +1,21 @@
 using UnityEditor;
-          using UnityEngine;
-          using System.Collections.Generic;
-          using System.IO;
-          using System;
+using System.IO;
 
-          public class BuildScript
-          {
-              static string[] GetScenePaths()
-              {
-                  // Trouver toutes les scènes dans Assets/Scenes
-                  List<string> scenes = new List<string>();
+public class BuildScript
+{
+    public static void Build()
+    {
+        string outputPath = "Builds/Android/mygame.apk";
+        if (!Directory.Exists("Builds/Android")) {
+            Directory.CreateDirectory("Builds/Android");
+        }
 
-                  // Ajouter explicitement les scènes que nous avons identifié
-                  scenes.Add("Assets/Scenes/MainMenu.unity");
-                  scenes.Add("Assets/Scenes/Level1.unity");
-                  scenes.Add("Assets/Scenes/Level2.unity");
+        BuildPlayerOptions buildOptions = new BuildPlayerOptions();
+        buildOptions.scenes = new[] { "Assets/Scenes/Main.unity" };
+        buildOptions.locationPathName = outputPath;
+        buildOptions.target = BuildTarget.Android;
+        buildOptions.options = BuildOptions.None;
 
-                  // Afficher les scènes trouvées dans le log
-                  Debug.Log("Scènes incluses dans le build:");
-                  foreach (string scene in scenes)
-                  {
-                      Debug.Log("- " + scene);
-                  }
-
-                  return scenes.ToArray();
-              }
-
-              [MenuItem("Build/Build Android")]
-              public static void BuildAndroid()
-              {
-                  Debug.Log("Démarrage du build Android...");
-                  
-                  // Configurer les paramètres Android
-                  PlayerSettings.Android.keystorePass = "";
-                  PlayerSettings.Android.keyaliasPass = "";
-                  
-                  // Définir un bundle ID valide
-                  PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.yourgame.angrybirdsremake");
-                  
-                  // Configurer la version minimale
-                  PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel22;
-                  
-                  // Définir le chemin de sortie
-                  string buildPath = Path.Combine(Application.dataPath, "../Builds/Android/AngryBirdsRemake.apk");
-                  
-                  // Créer le répertoire s'il n'existe pas
-                  Directory.CreateDirectory(Path.GetDirectoryName(buildPath));
-                  
-                  // Options de build
-                  BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
-                  {
-                      scenes = GetScenePaths(),
-                      locationPathName = buildPath,
-                      target = BuildTarget.Android,
-                      options = BuildOptions.None
-                  };
-                  
-                  // Afficher la configuration
-                  Debug.Log("Chemin de build: " + buildPath);
-                  Debug.Log("Nombre de scènes: " + buildPlayerOptions.scenes.Length);
-                  
-                  // Lancer le build
-                  BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-                  BuildSummary summary = report.summary;
-                  
-                  // Afficher les résultats
-                  Debug.Log("Résultat du build: " + summary.result);
-                  Debug.Log("Taille du build: " + summary.totalSize + " bytes");
-                  Debug.Log("Temps total: " + summary.totalTime.TotalSeconds + " secondes");
-                  
-                  // Vérifier si le build a réussi
-                  if (summary.result == BuildResult.Succeeded)
-                  {
-                      Debug.Log("Build réussi: " + buildPath);
-                      EditorApplication.Exit(0);
-                  }
-                  else
-                  {
-                      Debug.LogError("Build échoué!");
-                      EditorApplication.Exit(1);
-                  }
-              }
-          }
+        BuildPipeline.BuildPlayer(buildOptions);
+    }
+}
